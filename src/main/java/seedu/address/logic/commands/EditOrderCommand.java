a82fe33e899c92b8e44fab1fe50262544cc939e4
aaronsms <aaronsms@u.nus.edu>
2021-04-07 14:30:30 +0800

    Fix edit order index error message

--- src/main/java/seedu/address/logic/commands/EditOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/EditOrderCommand.java
@@ -72,7 +72,7 @@ public class EditOrderCommand extends EditCommand {
         List<Order> lastShownList = model.getFilteredOrderList();
 
         if (index.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_CHEESE_DISPLAYED_INDEX);
+            throw new CommandException(Messages.MESSAGE_INVALID_ORDER_DISPLAYED_INDEX);
         }
 
         Order orderToEdit = lastShownList.get(index.getZeroBased());
